3cc7cd02679f8723a95bed8a744d262b14f8d9d0
Shak Feizi <>
2024-12-30 14:04:11 -0600

    signin implemented on client side with api call

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -74,7 +74,7 @@ export default function SignUp() {
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Have and account?</span>
+            <span>Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
           </div>
           { errorMessage && (
